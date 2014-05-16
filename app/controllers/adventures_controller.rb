e6ac8bc51e803af8d3889fb0f45d6b9db338d002
courtney mcilhenny <github@courtneymcilhenny>
2014-05-15 17:05:02 -0700

    adventure GUID working, adventure index page up.

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,32 @@
 class AdventuresController < ApplicationController
+  # before_filter :load_library
+
+
+  def index
+    @adventures = Adventure.all
+  end
+
+  def new
+    @adventures = Adventure.new
+  end
+
+  def create 
+    adventure = Adventure.new adventure_params
+    adventure.Guid = SecureRandom.urlsafe_base64
+    adventure.save
+    redirect_to adventure_path
+  end
+
+  def show
+    @adventure = Adventure.find(params[:id])
+  end
+
+  private 
+    def adventure_params
+      params.require(:adventure).permit(:title, :author)
+    end
+
+    # def load_library
+    #   @library = Library.find(params[:library_id])
+    # end
 end
