a438da323ed61d246cf51b09f3fc4ac950760ac2
courtney mcilhenny <github@courtneymcilhenny>
2014-07-26 14:54:41 -0700

    adding case sensativity to linkifier.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -7,7 +7,7 @@ module PagesHelper
       #create an array with two seperated pieces 
       caps = regex.match(link)
       #variable that searches database for the "linked_to_page" by looking up the name of the page
-      linked_page = page.adventure.pages.where(:name => caps[2]).first
+      linked_page = page.adventure.pages.find(:first, :conditions => ["lower(name) = ?", caps[2].downcase])
       #creating link with "link text" poining to the next page of the adventure. THe next page was found by looking it up by name and pulling the id from this param.
       link_to(caps[1], adventure_page_path(page.adventure_id, linked_page.id))
     end
